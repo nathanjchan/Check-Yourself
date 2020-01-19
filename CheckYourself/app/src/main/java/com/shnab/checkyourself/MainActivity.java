c67b78844cf1353889a3326bc5eb2068d0972c96
SHNAB <cys@checkyourself.tech>
2020-01-19 09:26:22 +0000

    first commit

---
+++ CheckYourself/app/src/main/java/com/shnab/checkyourself/MainActivity.java
@@ -0,0 +1,60 @@
+package com.shnab.checkyourself;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    // Initialize Variable
+    ImageView imageView;
+    Button btOpen;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Assign variable
+        imageView = findViewById(R.id.image_view);
+        btOpen = findViewById(R.id.bt_open);
+
+        // Request for camera permission
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{
+                            Manifest.permission.CAMERA
+                    },
+                    100);
+        }
+
+        btOpen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Open camera
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, 100);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == 100) {
+            // Get capture image
+            Bitmap captureImage = (Bitmap) data.getExtras().get("data");
+            // Set capture image to ImageView
+            imageView.setImageBitmap(captureImage);
+        }
+    }
+}
